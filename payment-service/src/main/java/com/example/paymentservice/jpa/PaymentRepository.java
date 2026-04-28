67fdf9388c7460ba80b535289d4c415b2e0e47ae
edowon0623@gmail.com <Dwlee$09>
2026-04-29 08:18:57 +0900

    delete the unnecessary files

--- payment-service/src/main/java/com/example/paymentservice/jpa/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.paymentservice.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PaymentRepository extends CrudRepository<PaymentEntity, Long> {
-    PaymentEntity findByPaymentId(String productId);
-    PaymentEntity findByOrderId(String orderId);
-}
--- product-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into products(product_id, product_name, stock, unit_price)
-values('PRODUCT-001', 'Berlin', 100, 1500);
-insert into products(product_id, product_name, stock, unit_price)
-values('PRODUCT-002', 'Tokyo', 110, 1000);
-insert into products(product_id, product_name, stock, unit_price)
-values('PRODUCT-003', 'Stockholm', 120, 2000);
