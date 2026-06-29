cf850c9ffed82b27d35dc90bf451c6e283289db0
edowon0623@gmail.com <Dwlee$09>
2026-06-29 12:17:51 +0900

    change config-service project name to config-server

---
+++ ch15/config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.configserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ConfigServerApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
